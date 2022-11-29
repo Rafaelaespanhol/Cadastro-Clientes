079cc1231c5086017f493fc46cbf95254c8a0db7
Rafaela Espanhol <117127231+Rafaelaespanhol@users.noreply.github.com>
2022-11-28 22:14:09 -0300

    estrutura de repetição

---
+++ repetição.php
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+<h1> Repetição<h1>
+    <?php
+    $contador = 1;
+    while($contador < 10){
+        echo "contador com while loop numero $contador <br>";
+        $contador = $contador + 1;
+        
+    }
+
+    <Title>Repetição</title>
+ </head>
+ <body>
+
+ <h1>Repetição<h1>
+ <?php
+ //array
+ $clientes = ["rafa,"cleyton,"Renata,"pedro,"Andrea,"Ana];
+ $contador = 0;
+ while(contador <= 6){
+echo "$clientes[$contador]<br>";
+$contador = $contador + 1;
+
+ }
+
